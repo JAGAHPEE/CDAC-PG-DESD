f1ecaf821bafd65286842894fccb3613065edc16
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-09-15 14:54:47 +0530

    Update Pascal_triangle.c

--- C Program/C Assignmant/Sept_15/Pattern/Pascal_triangle.c
+++ C Program/C Assignmant/Sept_15/Pattern/Pascal_triangle.c
@@ -1,1 +1,22 @@
 
+#include<stdio.h>
+int main()
+{   int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int k=n;k>i;k--)
+        {
+            printf(" ");
+        }
+        int num=1;
+        for(int j=1;j<=i;j++)
+        {
+            printf( " %d",num);
+            num = num*(i-j)/j;
+
+        }
+        printf("\n");
+       
+    }
+}
