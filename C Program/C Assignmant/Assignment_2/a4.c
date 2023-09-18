110461df16b72e1eb99d6613338bdf814e065663
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-09-18 23:29:01 +0530

    Add files via upload

---
+++ C Program/C Assignmant/Assignment_2/a4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter Value : ");
+    scanf("%d",&num);
+    int sum=0;
+
+    for(int i=1;i<num+1;i++)
+    {
+        sum = sum+i;
+    }
+    printf("Sum of %d Natural No. : %d",num,sum);
+    
+
+}
