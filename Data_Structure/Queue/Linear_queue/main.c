ad311dae0d705ec96c8e9b8b6e2c4f7b3a81d105
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-10-06 17:25:46 +0530

    Update main.c

--- Data_Structure/Queue/Linear_queue/main.c
+++ Data_Structure/Queue/Linear_queue/main.c
@@ -1,3 +1,5 @@
+/* Owner :- Sandesh Kale  */
+
 #include<stdio.h>
 #include<stdlib.h>
 struct node
@@ -88,6 +90,7 @@ int deque()
 	}
 	return x;
 }
+/* Owner :- Sandesh Kale  */
 void display()
 {
 	if(!underflow())
