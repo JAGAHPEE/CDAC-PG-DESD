f0505c882c353ca8c4aed8988c073caa4a826338
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-10-06 16:01:01 +0530

    Update main.c

--- Data_Structure/Link_List/main.c
+++ Data_Structure/Link_List/main.c
@@ -11,31 +11,83 @@ struct node *start;
 
 void insert_Beg(int ele);
 void insert_End(int ele);
-void insert_After(int ele,int n);
-void insert_Before(int ele,int s);
+void insert_After(int ele, int n);
+void insert_Before(int ele, int s);
 int delete_First();
 int delete_Last();
-void delete_Sp(int, int);
+void delete_Sp(int);
 void traverse_Forward();
 void traverse_Backward();
+void print_func(struct node *);
+void reverse();
 
 int main()
 {
-	insert_Beg(11);
-	traverse_Forward();
-	printf("\n");
-	insert_Beg(10);
-	insert_Beg(12);
-	traverse_Forward();
-	printf("\n");
-	delete_First();
-	insert_End(10);
-	traverse_Forward();
-	printf("\n");
-	insert_After(20,11);
-	traverse_Forward();
-	insert_Before(20,11);
-	printf("\n");
+	int option;
+	while(1)
+	{
+		
+		printf("\nPress 1 to Insert Beginning\n");
+		printf("Press 2 to Insert End\n");
+		printf("Press 3 to Insert After\n");
+		printf("Press 4 to Insert Before\n");
+		printf("Press 5 to Delete First\n");
+		printf("Press 6 to Delete Last\n");
+		printf("Press 7 to Delete Specific\n");
+		printf("Press 8 to Travel Forward\n");
+		printf("Press 9 to Travel Reverse\n");
+		printf("Press 0 to Reverse List\n");		
+		printf("Enter Option: ");
+		scanf("%d",&option);
+		int n;
+		switch(option)
+		{
+			case 1:
+				printf("\nEnter the element to Insert:");
+				scanf("%d",&n);
+				insert_Beg(n);
+				break;
+			case 2:
+				printf("\nEnter the element to Insert:");
+				scanf("%d",&n);
+				insert_End(n);
+				break;
+			case 3:
+				printf("\nEnter the element to Insert After:");
+				scanf("%d",&n);
+				insert_After(10,n);
+				break;
+			case 4:
+				printf("\nEnter the element to Insert Before:");
+				scanf("%d",&n);
+				insert_Before(10,n);
+				break;
+			case 5:
+				delete_First();
+				break;
+			case 6:
+				delete_Last();
+				break;
+			case 7:
+				printf("\nEnter the element for Delete Specific:");
+				scanf("%d",&n);
+				delete_Sp(10);
+				break;
+			case 8:
+				traverse_Forward();
+				break;
+			case 9:
+				traverse_Backward();
+				break;
+			case 0:
+				reverse();
+				break;
+			default:
+				printf("Invalid Options--\n");
+				exit(1);
+				break;
+		}	
+	}
 }
 void insert_Beg(int ele)
 {
@@ -78,18 +130,18 @@ void insert_End(int ele)
 void insert_After(int ele, int s)
 {
 	struct node *current, *temp;
-	if(start!=NULL)
+	if (start != NULL)
 	{
 		current = start;
-		while(current!=NULL && current->data !=s)
+		while (current != NULL && current->data != s)
 		{
 			current = current->next;
 		}
-		if(current!=NULL)
+		if (current != NULL)
 		{
 			temp = (struct node *)malloc(sizeof(struct node));
 			temp->data = ele;
-			temp ->next = current->next;
+			temp->next = current->next;
 			current->next = temp;
 		}
 		else
@@ -103,75 +155,54 @@ void insert_After(int ele, int s)
 	}
 }
 
-void insert_Before(int ele,int s)
+void insert_Before(int ele, int s)
 {
-	struct node *temp,*current,*prev;
-	if(start!=NULL)
-	{	
+	struct node *temp, *current, *prev;
+	if (start != NULL)
+	{
 		current = start;
-  prev = current ;
-		while (current!=NULL && current->data!=s)
-		{
-			prev = current;
-			current = current->next;
-		}
-		if(current!=NULL)
+		if (current->data == s)
 		{
 			temp = (struct node *)malloc(sizeof(struct node));
 			temp->data = ele;
-			temp->next = current;
-			prev->next = temp;
-
+			temp->next = start;
+			start = temp;
 		}
 		else
 		{
-			printf("Element not found");
+
+			current = start;
+			while (current->next != NULL && current->next->data != s)
+			{
+				current = current->next;
+			}
+			if (current->next != NULL)
+			{
+				temp = (struct node *)malloc(sizeof(struct node));
+				temp->data = ele;
+				temp->next = current->next;
+				current->next = temp;
+			}
+			else
+			{
+				printf("Element not found");
+			}
 		}
-		
 	}
 	else
 	{
 		printf("List is Empty");
 	}
-
 }
-
-// void insert_After(int ele, int s)
-// {
-// 	struct node *current, *temp;
-// 	if (start == NULL)
-// 	{
-// 		printf("List is Empty");
-// 	}
-// 	else
-// 	{
-// 		current = start;
-// 		while (current != NULL && current->data != s)
-// 		{
-// 			current = current->next;
-// 		}
-// 		if (current != NULL)
-// 		{
-// 			temp = (struct node *)malloc(sizeof(struct node));
-// 			temp->data = ele;
-// 			temp->next = current->next;
-// 			current->next = temp;
-// 		}
-// 		else
-// 		{
-// 			printf("Element not Found");
-// 		}
-// 	}
-// }
 
 int delete_First()
 {
 	int x;
-	if(start!=NULL)
+	if (start != NULL)
 	{
 
 		struct node *temp;
-		temp = (struct node*)malloc(sizeof(struct node));
+		temp = (struct node *)malloc(sizeof(struct node));
 		temp = start;
 		x = temp->data;
 		start = temp->next;
@@ -192,4 +223,104 @@ void traverse_Forward()
 		printf("%d ", current->data);
 		current = current->next;
 	}
+}
+void delete_Sp(int s)
+{
+	struct node *temp, *current;
+	if (start != NULL)
+	{
+		current = start;
+
+		if (current->data == s)
+		{
+			start = current->next;
+			free(current);
+		}
+		else
+		{
+			while (current->next != NULL && current->next->data != s)
+			{
+				current = current->next;
+			}
+			if (current->next != NULL)
+			{
+				temp = (struct node *)malloc(sizeof(struct node));
+				temp = current->next;
+				current->next = temp->next;
+				free(temp);
+				temp = NULL;
+			}
+			else
+			{
+				printf("Element not found");
+			}
+		}
+	}
+	else
+	{
+		printf("List is empty");
+	}
+}
+int delete_Last()
+{
+	struct node *temp, *current;
+	int x = -1;
+	if (start != NULL)
+	{
+		if (start->next == NULL)
+		{
+			x = start->data;
+			free(start);
+			start = NULL;
+		}
+		else
+		{
+			current = start;
+			while (current->next->next != NULL)
+			{
+				current = current->next;
+			}
+			x = current->next->data;
+			free(current->next);
+			current->next = NULL;
+		}
+	}
+	else
+	{
+		printf("List is Empty");
+	}
+	return x;
+}
+void print_func(struct node *current)
+{
+	if (current == NULL)
+	{
+		return;
+	}
+	print_func(current->next);
+	printf("%d ", current->data);
+}
+void traverse_Backward()
+{
+	if (start != NULL)
+	{
+		print_func(start);
+	}
+	else
+	{
+		printf("List is Empty");
+	}
+}
+void reverse()
+{
+	struct node *temp, *rev;
+	rev = NULL;
+	while (start != NULL)
+	{
+		temp = start;
+		start = temp->next;
+		temp->next = rev;
+		rev = temp;
+	}
+	start = rev;
 }
