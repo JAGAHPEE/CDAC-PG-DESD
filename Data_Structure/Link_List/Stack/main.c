58820f6269cd5f62b8c23e68464b0ae33400edfe
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-10-08 17:24:46 +0000

    modified:   Data_Structure/Link_List/Stack/main.c

--- Data_Structure/Link_List/Stack/main.c
+++ Data_Structure/Link_List/Stack/main.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
     int data;
     struct node *next;
 };
-struct node *start=NULL;
-struct node *last=NULL;
-struct node *push(struct node*);
-struct node *pop(struct node*);
-struct node *display(struct node*);
+struct node *start = NULL;
+struct node *last = NULL;
+struct node *push(struct node *);
+struct node *pop(struct node *);
+struct node *display(struct node *);
 
 int main()
 {
@@ -21,7 +22,7 @@ int main()
         printf("\nPress 4 to DISPLAY");
         printf("\nPress 5 to EXIT");
         printf("\nEnter the Options: ");
-        scanf("%d",&option);
+        scanf("%d", &option);
         switch (option)
         {
         case 1:
@@ -31,45 +32,95 @@ int main()
             start = pop(start);
             break;
         case 3:
-            if(last!=NULL)
-                printf("Last Element Inserted: %d",last->data);
+            if (last != NULL)
+                printf("Last Element Inserted: %d", last->data);
+            else
+                printf("No Element Inserted");
             break;
         case 4:
             start = display(start);
             break;
         default:
-            return;
+            return 0;
             break;
         }
     }
-    
-
 }
 
 struct node *push(struct node *start)
 {
     int num;
-    struct node *ptr,*new_node;
+    struct node *ptr, *new_node;
     printf("\nEnter the Number: ");
-    scanf("%d",&num);
-    ptr= start;
-    new_node = (struct node*)malloc(sizeof(struct node));
+    scanf("%d", &num);
+    ptr = start;
+    new_node = (struct node *)malloc(sizeof(struct node));
     new_node->data = num;
-    if(ptr==NULL)
+    if (ptr == NULL)
     {
         start = new_node;
-        new_node->next=NULL; 
+        new_node->next = NULL;
     }
     else
     {
-        ptr= start;
-        while (ptr->next!=NULL)
+        ptr = start;
+        while (ptr->next != NULL)
         {
             ptr = ptr->next;
         }
         ptr->next = new_node;
         new_node->next = NULL;
         last = new_node;
+    }
+    return start;
+}
+struct node *display(struct node *start)
+{
+    struct node *ptr;
+    ptr = start;
+    if (ptr != NULL)
+    {
+        while (ptr != NULL)
+        {
+            printf("%d ", ptr->data);
+            ptr = ptr->next;
+        }
+    }
+    else
+    {
+        printf("Stack is Empty");
+    }
+    return start;
+}
+
+struct node *pop(struct node *start)
+{
+    struct node *ptr;
+    ptr = start;
+    if (ptr != NULL)
+    {
+
+        if (ptr->next == NULL)
+        {
+            ptr = (struct node *)malloc(sizeof(struct node *));
+            ptr  = start;
+            start = start->next;
+            free(ptr);
+        }
+        else{
+            ptr = start;
+            while(ptr->next->next!=NULL)
+            {
+                ptr = ptr->next;
+            }
+            free(ptr->next);
+            ptr->next=NULL;
+            last = ptr;
+        }
+    }
+    else
+    {
+        printf("Stack Underflow");
     }
     return start;
 }
