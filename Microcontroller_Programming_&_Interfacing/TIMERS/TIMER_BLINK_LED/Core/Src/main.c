e746528086494982780df66c310011c220b9fb02
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-11-03 23:31:18 +0530

    Update main.c

--- Microcontroller_Programming_&_Interfacing/TIMERS/TIMER_BLINK_LED/Core/Src/main.c
+++ Microcontroller_Programming_&_Interfacing/TIMERS/TIMER_BLINK_LED/Core/Src/main.c
@@ -1,18 +1,8 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  * Published By :- Sandesh Kale
+  * Github	:- github/JAGAHPEE
   ******************************************************************************
   */
 /* USER CODE END Header */
