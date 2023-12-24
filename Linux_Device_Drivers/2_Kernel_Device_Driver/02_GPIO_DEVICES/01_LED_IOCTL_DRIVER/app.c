d542d74269f3dcd79fbfe415a4a4cc659182d519
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-12-24 22:11:43 +0530

    Add files via upload

---
+++ Linux_Device_Drivers/2_Kernel_Device_Driver/02_GPIO_DEVICES/01_LED_IOCTL_DRIVER/app.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/ioctl.h>
+#include"headers.h"
+
+int main()
+{
+	int fd = open("/dev/GPIO_device_driver",O_RDWR),option = 0 ;
+	if(fd<0)
+	{
+		printf("Unable to open file \n");
+		return -1;
+	}
+	while(1)
+	{
+		printf("Press 1 for LED ON and 0 for LED OFF\n");
+		scanf("%d",&option);
+		if(option==1)
+		{
+			if(ioctl(fd,GPIO_SET_L,&option))
+			{
+				printf("Unable to Send Command\n");
+			}
+			else
+			{
+				printf("Command Sent\n");
+			}
+		}
+		else if(option ==0)
+		{
+			if(ioctl(fd,GPIO_RESET_L,&option))
+			{
+				printf("Unable to Send Command\n");
+			}
+			else
+			{
+				printf("Command Sent\n");
+			}
+		}
+		else
+		{
+			printf("Invalid Options\n");
+		}		
+	}				
+	close(fd);
+}
+
---
+++ Linux_Device_Drivers/2_Kernel_Device_Driver/02_GPIO_DEVICES/01_LED_IOCTL_DRIVER/headers.h
@@ -0,0 +1,3 @@
+#define MAGIC_NO 'k'
+#define GPIO_SET_L _IOW(MAGIC_NO,0,int)
+#define GPIO_RESET_L _IOW(MAGIC_NO,1,int)
